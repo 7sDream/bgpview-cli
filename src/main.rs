2cd91b6a9eaf97f230b4c7816dd29016137f41f8
7sDream <i@7sdre.am>
2020-07-20 15:27:41 +0800

    feat(ip): add rir, iana and maxmind location info

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ use args::Args;
 
 pub use anyhow::Result;
 
+fn unwrap_or_unknown<S: AsRef<str>>(s: &Option<S>) -> &str {
+    s.as_ref().map(|s| s.as_ref()).unwrap_or("Unknown")
+}
+
 fn main() -> Result<()> {
     let argument = args::get();
     let client = client::Client::new()?;
@@ -23,23 +27,46 @@ fn main() -> Result<()> {
     match argument {
         Args::Ip { ip } => {
             let data = client.ip(&ip)?;
-            let prefixes = &data.prefixes;
 
+            println!("     IP: {}", ip);
+
+            let loc = &data.max_mind;
+
+            println!("Country: {}", loc.country_code);
+            println!("   City: {}", unwrap_or_unknown(&loc.city));
+
+            let iana = &data.iana_assignment;
+            println!();
+            println!("   IANA: {}", iana.description);
+            println!("AssigSt: {}", iana.assignment_status);
+            println!("AssigAt: {}", unwrap_or_unknown(&iana.data_assigned));
+            println!("  Whois: {}", iana.whois_server);
+
+            let rir = &data.rir_allocation;
+            println!();
+            println!("    RIR: {} - {}", rir.rir_name, rir.country_code);
+            println!(" Prefix: {}", rir.prefix);
+            println!("AllocSt: {}", rir.allocation_status);
+            if let Some(ref date) = rir.date_allocated {
+                println!("AllocAt: {}", date);
+            }
+
+            let prefixes = &data.prefixes;
             if prefixes.is_empty() {
+                println!();
                 println!("No ASN owns {}", ip);
             } else {
-                println!("     IP: {}", ip);
                 for prefix in prefixes {
-                    let asn = &prefix.asn;
                     println!();
+                    let asn = &prefix.asn;
                     println!(
                         "    ASN: {} - {} - {} - {}",
                         asn.asn, asn.name, asn.description, asn.country_code,
                     );
                     println!(" Prefix: {}", prefix.prefix);
-                    println!("   Name: {}", prefix.name);
-                    println!("   Desc: {}", prefix.description);
-                    println!("Country: {}", prefix.country_code);
+                    println!("   Name: {}", unwrap_or_unknown(&prefix.name));
+                    println!("   Desc: {}", unwrap_or_unknown(&prefix.description));
+                    println!("Country: {}", unwrap_or_unknown(&prefix.country_code));
                 }
             }
         }
--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,50 @@
-use serde::{Deserialize, Serialize};
+use {
+    serde::de,
+    serde::Deserialize,
+    std::fmt::{self, Formatter},
+};
 
-#[derive(Serialize, Deserialize)]
+struct DeserializeCIDR;
+
+impl<'de> serde::de::Visitor<'de> for DeserializeCIDR {
+    type Value = u16;
+
+    fn expecting(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
+        formatter.write_str("an cidr number or string which has range [0, 128]")
+    }
+
+    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if v > 128 {
+            Err(E::invalid_value(de::Unexpected::Unsigned(v), &self))
+        } else {
+            Ok(v as u16)
+        }
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if v == "" {
+            Ok(0)
+        } else {
+            v.parse()
+                .map_err(|_| E::invalid_value(de::Unexpected::Str(v), &self))
+        }
+    }
+}
+
+fn deserialize_cidr<'de, D>(deserializer: D) -> Result<u16, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    deserializer.deserialize_any(DeserializeCIDR)
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Asn {
     pub asn: u64,
     pub name: String,
@@ -8,24 +52,55 @@ pub struct Asn {
     pub country_code: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Prefix {
     pub prefix: String,
     pub ip: String,
-    pub cidr: u8,
+    #[serde(deserialize_with = "deserialize_cidr")]
+    pub cidr: u16,
     pub asn: Asn,
-    pub name: String,
-    pub description: String,
-    pub country_code: String,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub country_code: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
+pub struct RIRAllocation {
+    pub rir_name: String,
+    pub country_code: String,
+    pub ip: String,
+    #[serde(deserialize_with = "deserialize_cidr")]
+    pub cidr: u16,
+    pub prefix: String,
+    pub allocation_status: String,      // TODO: maybe a enum?
+    pub date_allocated: Option<String>, // TODO: maybe use chrono::Date someday?
+}
+
+#[derive(Debug, Deserialize)]
+pub struct IANAAssignment {
+    pub assignment_status: String,
+    pub description: String,
+    pub whois_server: String,
+    pub data_assigned: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MaxMind {
+    pub country_code: String,
+    pub city: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct IpResponseData {
     pub ip: String,
     pub prefixes: Vec<Prefix>,
+    pub rir_allocation: RIRAllocation,
+    pub iana_assignment: IANAAssignment,
+    #[serde(rename = "maxmind")]
+    pub max_mind: MaxMind,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Response<Data> {
     pub status: String,
     pub status_message: String,
